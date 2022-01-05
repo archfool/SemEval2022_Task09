a47bd9248d4a2e849a3e36b166c36b7a7e644cc5
archfool <archfool@github.com>
2022-01-05 21:22:38 +0800

    220105

--- data_process.py
+++ data_process.py
@@ -5,6 +5,8 @@ author: ruanzhihao_archfool
 """
 
 import os
+import pandas as pd
+import numpy as np
 from conllu import parse
 from init_config import src_dir, data_dir
 
@@ -73,6 +75,77 @@ ID   FORM    LEMMA   CPOSTAG POSTAG  FEATS   HEAD    DEPREL  PHEAD   PDEPREL
 12-20. ARGX: The arguments of the X-th predicate in current sentence.
 """
 
+"""
+We adopt the concept of "question families" as outlined in the CLEVR dataset (Johnson et al., 2017). While some 
+question families naturally transfer over from the VQA domain (e.g., integer comparison, counting), other concepts 
+such as ellipsis and object lifespan must be employed to cover the full extent of competency within procedural 
+texts.
+"""
+
+
+def parse_recipe(recipe):
+    len(recipe[0].metadata)
+    # 提取QA数据
+    q_list = []
+    a_list = []
+    for key, value in recipe[0].metadata.items():
+        if key.__contains__('question'):
+            q_list.append((key, value))
+        elif key.__contains__('answer'):
+            a_list.append((key, value))
+    assert len(q_list) == len(a_list)
+    for key, value in q_list + a_list:
+        recipe[0].metadata.pop(key)
+    qa_list = []
+    for (q_key, q_value), (a_key, a_value) in zip(q_list, a_list):
+        q_id = q_key.split(' ')[1]
+        a_id = a_key.split(' ')[1]
+        assert q_id == a_id
+        cat_id, seq_id = q_id.split('-')
+        qa_list.append((cat_id, seq_id, q_value, a_value))
+    qa_df = pd.DataFrame(qa_list, columns=['cat_id', 'seq_id', 'question', 'answer'])
+    qa_df['cat_id'] = qa_df['cat_id'].astype(int)
+    qa_df['seq_id'] = qa_df['seq_id'].astype(int)
+    qa_df = qa_df.sort_values(['cat_id', 'seq_id']).reset_index(drop=True)
+
+    # 提取元数据
+    metadata = {}
+    for key, value in recipe[0].metadata.items():
+        if key.__contains__('metadata'):
+            metadata[key.split(':')[-1]] = value
+    for key in metadata.keys():
+        recipe[0].metadata.pop('metadata:' + key)
+    # 提取菜谱ID
+    # metadata['newdoc_id'] = recipe[0].metadata.pop('newdoc id')
+    newdoc_id = recipe[0].metadata.pop('newdoc id')
+    # 分流食材清单和工序步骤
+    ingredients = []
+    directions = []
+    for sent in recipe:
+        if 'sent_id' not in sent.metadata.keys():
+            # print(metadata['url'])
+            # todo 验证集中的metadata的sent，都是ingredients类型的，有时间的话要对训练集做验证
+            ingredients[-1].extend(sent)
+            pass
+        elif 'ingredients' == sent.metadata['sent_id'].split('::')[1]:
+            ingredients.append(sent)
+        else:
+            directions.append(sent)
+    # 原始菜谱的文本长度
+    metadata['seq_len'] = sum([len(x) for x in ingredients]) + sum([len(x) for x in directions])
+    # 返回新结构的菜谱
+    new_recipe = {
+        'newdoc_id': newdoc_id,
+        'metadata': metadata,
+        'qa_df': qa_df,
+        'ingredients': ingredients,
+        'ingredient_dfs': [pd.DataFrame([x for x in ingredient]) for ingredient in ingredients],
+        'directions': directions,
+        'direction_dfs': [pd.DataFrame([x for x in direction]) for direction in directions],
+    }
+    return new_recipe
+
+
 if __name__ == "__main__":
     data_val_dir = os.path.join(data_dir, 'val')
     data_train_dir = os.path.join(data_dir, 'train')
@@ -88,12 +161,16 @@ if __name__ == "__main__":
         data = data.split('\n\n\n')
         data = [parse(d, fields=fields) for d in data]
 
-    for d in data:
-        for sent in d:
-            for idx in range(len(sent)):
-                token = sent[idx]
-                if 10 != len(token):
-                    print(token)
-
+    recipes = []
+    for recipe in data:
+        recipes.append(parse_recipe(recipe))
+        # for sent in recipe:
+        #     for token in sent:
+        #         if 10 != len(token):
+        #             print(token)
+    for recipe in recipes:
+        print(recipe['metadata']['seq_len'])
+    if True:
+        qa_all = pd.concat([r['qa_df'] for r in recipes]).sort_values(['cat_id', 'seq_id']).reset_index(drop=True)
+        qa_all.to_csv(os.path.join(data_dir, 'qa_val.csv'), index=None, sep='\t', encoding='utf-8')
     print('END')
-
